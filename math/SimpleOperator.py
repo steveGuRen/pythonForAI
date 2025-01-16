eb3e9e20a0e3f8ff4399df958d0a80e46bef341d
steveGuRen <529543738@qq.com>
2025-01-16 15:33:51 +0800

    add math test

---
+++ math/SimpleOperator.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# 创建数组
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# 加法
+add = a + b  # 或 np.add(a, b)
+
+# 减法
+sub = a - b  # 或 np.subtract(a, b)
+
+# 乘法
+mul = a * b  # 或 np.multiply(a, b)
+
+# 除法
+div = a / b  # 或 np.divide(a, b)
+
+print(add, sub, mul, div)
