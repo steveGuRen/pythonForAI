d2549e8cffe2dfc7fbc2453855356f76c58d49a0
steveGuRen <529543738@qq.com>
2025-01-23 15:55:05 +0800

    增加显示双坐标轴柱状图的能力

--- bar/dual-axes-bar.py
+++ bar/dual-axes-bar.py
@@ -13,15 +13,15 @@ x = np.arange(len(categories))  # x轴位置
 fig, ax1 = plt.subplots()
 
 # 绘制主坐标轴的柱状图
-ax1.bar(x - 0.2, value1, 0.4, label='Value 1', color='skyblue')
+ax1.bar(x, value1, 0.2, label='Value 1', color='skyblue')
 ax1.set_xlabel('Category')
 ax1.set_ylabel('Primary Axis', color='skyblue')
 ax1.tick_params(axis='y', labelcolor='skyblue')
 
 # 创建副坐标轴
 ax2 = ax1.twinx()  # 共享x轴
-ax2.bar(x + 0.2, value2, 0.4, label='Value 2', color='orange', alpha=0.7)
-ax2.bar(x + 0.6, value3, 0.4, label='Value 3', color='green', alpha=0.7)
+ax2.bar(x + 0.2, value2, 0.2, label='Value 2', color='orange', alpha=0.7)
+ax2.bar(x + 0.4, value3, 0.2, label='Value 3', color='green', alpha=0.7)
 ax2.set_ylabel('Secondary Axis', color='orange')
 ax2.tick_params(axis='y', labelcolor='orange')
 
@@ -30,7 +30,7 @@ ax1.set_xticks(x)
 ax1.set_xticklabels(categories)
 
 # 设置图例
-fig.legend(loc='upper left', bbox_to_anchor=(0.1, 0.9))
+fig.legend(loc='upper left', bbox_to_anchor=(0.15, 0.8))
 
 # 显示图形
 plt.title("Bar Plot with Dual Axes")
